7456c13aa75ba7104dda937a63563ffb08999d34
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-11 02:41:27 -0400

    object class

---
+++ src/ImpJava/NewProject/object.java
@@ -0,0 +1,14 @@
+package ImpJava.NewProject;
+
+import com.sun.tools.javac.Main;
+
+public class object {
+    int X = 5;
+
+    public static void main (String [] arg){
+        //Object adding a main class
+        object myObj = new object();
+        //display the object
+        System.out.println(myObj.X);
+    }
+}
