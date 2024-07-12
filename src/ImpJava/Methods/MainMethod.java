1360910ad6c55a0f44638b58475175fc266e757f
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-12 02:35:58 -0400

    Mathod in java program

---
+++ src/ImpJava/Methods/MainMethod.java
@@ -0,0 +1,10 @@
+package ImpJava.Methods;
+
+public class MainMethod {
+    static void myMethod (){
+        System.out.println("I just got executed!");
+    }
+    public static void main(String []arg){
+        myMethod();
+    }
+}
