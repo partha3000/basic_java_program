7d48be36c6f2d638163c0793e325d1da6fef94b6
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-13 02:07:18 -0400

    method java program

---
+++ src/ImpJava/Methods/myMethod.java
@@ -0,0 +1,12 @@
+package ImpJava.Methods;
+
+public class myMethod {
+    static void myMethod(String fname){
+        System.out.println(fname+ "Refsnes");
+    }
+    public static void main(String [] args){
+        myMethod("Lima");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
