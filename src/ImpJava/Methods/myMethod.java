6c9291fd530836457ce2e51dd3ef3ec3794e20a1
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-13 02:29:33 -0400

    method add name and age

--- src/ImpJava/Methods/myMethod.java
+++ src/ImpJava/Methods/myMethod.java
@@ -1,12 +1,14 @@
 package ImpJava.Methods;
 
 public class myMethod {
-    static void myMethod(String fname){
-        System.out.println(fname+ "Refsnes");
+    static void myMethod(String Name, int age){
+        //System.out.println(Name); // just one value
+        System.out.print(Name  + " is " +age +"" + " " ); // name age
     }
     public static void main(String [] args){
-        myMethod("Lima");
-        myMethod("Jenny");
-        myMethod("Anja");
+        myMethod("Rohan",21 );
+        myMethod("jit",22);
+        myMethod(" Rahul",22);
+        myMethod("Sandipon", 23);
     }
 }
