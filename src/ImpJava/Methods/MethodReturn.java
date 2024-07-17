a436c9b21d72dc99df139750c68620b6e308f331
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-16 23:08:48 -0400

    Hello deaar

---
+++ src/ImpJava/Methods/MethodReturn.java
@@ -0,0 +1,12 @@
+package ImpJava.Methods;
+
+public class MethodReturn {
+    static int myMethod(int x , int y){
+        return y + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(5, 3));
+    }
+}
+
