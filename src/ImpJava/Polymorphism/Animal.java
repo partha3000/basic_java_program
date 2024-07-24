69f315e1cff1312200ad0b982d057afda670f81d
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-23 23:48:06 -0400

    Adding a new project

--- src/ImpJava/Polymorphism/Animal.java
+++ src/ImpJava/Polymorphism/Animal.java
@@ -1,19 +1,22 @@
 package ImpJava.Polymorphism;
 
 public class Animal {
+    public static void main(String [] args){
+
+    }
     public void animalSound() {
         System.out.println("The animal makes a sound");
     }
-}
 
-class Pig extends Animal {
-    public void animalSound() {
-        System.out.println("The pig says: wee wee");
+    class Pig extends Animal {
+        public void animalSound() {
+            System.out.println("The pig says: wee wee");
+        }
     }
-}
 
-class Dog extends Animal {
-    public void animalSound() {
-        System.out.println("The dog says: bow wow");
+    class Dog extends Animal {
+        public void animalSound() {
+            System.out.println("The dog says: bow wow");
+        }
     }
 }
