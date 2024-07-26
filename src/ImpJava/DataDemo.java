249ffd062d3c259162afc0b13f35fa097c587323
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-26 03:37:32 -0400

    exercise 4

--- src/ImpJava/DataDemo.java
+++
@@ -1,7 +0,0 @@
-package ImpJava;
-
-public class DataDemo {
-    public static void main(String[] args) {
-        System.out.println("Mr");
-    }
-}
---
+++ src/javaProject/Exercise_in_java/Exercise4.java
@@ -0,0 +1,27 @@
+/*Write a java program to print the results of the following operations
+Test Data
+1. -5+8*6
+2. (55+9)%9
+3. 20+ -3*5/8
+4. 5+15/3*2-8%3
+
+
+*/
+
+package javaProject.Exercise_in_java;
+
+public class Exercise4 {
+    public static void main(String [] arg){
+        int a = -5+8*6;
+        int b = (55+9)%9;
+        int c = 20+ -3*5/8;
+        int d = 5+15/3*2-8%3;
+
+        System.out.println("Enter the result A is:"+a);
+        System.out.println("Enter the result A is:"+b);
+        System.out.println("Enter the result A is:"+c);
+        System.out.println("Enter the result A is:"+d);
+
+
+    }
+}
