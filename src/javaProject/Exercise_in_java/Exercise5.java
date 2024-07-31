421ed283911716822d1c0c1f905fee67b9968fcd
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-31 00:02:18 -0400

    hello dear

--- src/javaProject/Exercise_in_java/Exercise5.java
+++ src/javaProject/Exercise_in_java/Exercise5.java
@@ -7,14 +7,29 @@ input second number:5*/
 
 package javaProject.Exercise_in_java;
 
+import java.util.Scanner;
+
 public class Exercise5 {
     public static void main(String args[]){
 
-        int firstNumber = 25;
-        int secondNumber = 5;
-        int result = (firstNumber*secondNumber);
+        //one time solution
+//        int firstNumber = 25;
+//        int secondNumber = 5;
+//        int result = (firstNumber*secondNumber);
+//        System.out.print("Enter the two number is:"+result);
 
-        System.out.print("Enter the two number is:"+result);
+        //two time solution
+        //Using the scanner class
+        Scanner  scanner = new Scanner(System.in);
 
+        System.out.println("Enter the first number:");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int number2 = scanner.nextInt();
+
+        System.out.println(number1 +"x" +number2 +"="+ number1*number2);
+        //display function in java program
+        scanner.close(); //close the scanner
     }
 }
---
+++ src/javaProject/Exercise_in_java/Exercise6.java
@@ -0,0 +1,4 @@
+package javaProject.Exercise_in_java;
+
+public class Exercise6 {
+}
