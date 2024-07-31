04ce75687b2f45a7aec87e127c4ea96c89c24023
Partha Majumder <parthamajumder9810@gmail.com>
2024-07-30 23:49:10 -0400

    Enter the two numner

---
+++ src/javaProject/Exercise_in_java/Exercise5.java
@@ -0,0 +1,20 @@
+//Write a java program that takes two number as input and display the product of two number
+
+/*Test Date:07-30-2024
+Input first number:25
+input second number:5*/
+
+
+package javaProject.Exercise_in_java;
+
+public class Exercise5 {
+    public static void main(String args[]){
+
+        int firstNumber = 25;
+        int secondNumber = 5;
+        int result = (firstNumber*secondNumber);
+
+        System.out.print("Enter the two number is:"+result);
+
+    }
+}
