b24bd66ffe57c0d38be7942d9efc47f11c29bc68
Partha Majumder <parthamajumder9810@gmail.com>
2024-08-04 23:35:12 +0530

    Circal

--- src/javaProject/Exercise_in_java/Exercise10.java
+++ src/javaProject/Exercise_in_java/Exercise10.java
@@ -6,19 +6,35 @@ test date: 03-08-2024
 
 package javaProject.Exercise_in_java;
 
+import java.util.Scanner;
+
 public class Exercise10 {
 
-    private static final double radius = 7.5;
+//    private static final double radius = 7.5;
 
     public static void main (String [] arg){
+//
+//        System.out.println("Circle Perimeter and Area");
+//
+//        double perimeter = 2* Math.PI * radius;
+//
+//        double area = Math.PI * radius * radius;
+//
+//        System.out.println("Perimeter is = "+perimeter);
+//        System.out.println("Area is =" + area);
 
-        System.out.println("Circle Perimeter and Area");
 
-        double perimeter = 2* Math.PI * radius;
+        //using for scanner function
 
-        double area = Math.PI * radius * radius;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Perimeter is = "+perimeter);
-        System.out.println("Area is =" + area);
+        System.out.println("Inter the radius of the circle :");
+
+        double radius = scanner.nextDouble();
+
+        System.out.println("Perimeter is ="+(2 * radius *Math.PI));
+        System.out.println("Area is = "+ (Math.PI * radius * radius));
+
+        scanner.close();
     }
 }
