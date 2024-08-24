5812d134f69e4388b0cfcd5e2cacda7efcf38e99
Partha Majumder <parthamajumder9810@gmail.com>
2024-08-24 22:18:06 +0530

    Simpal Program

--- src/Convert_an_array/Examples03.java
+++ src/Convert_an_array/Examples03.java
@@ -2,6 +2,9 @@ package Convert_an_array;
 
 public class Examples03 {
     public static void main(String [] args){
-        System.out.println("Enter the new program ");
+        System.out.println("Wel-come");
+        System.out.println("HE-JE");
+        System.out.println("FE-DD");
+        System.out.println("ID ");
     }
 }
