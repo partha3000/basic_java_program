b142a60339f01001c5639bd7ed1ae2300072d8ff
Partha Majumder <parthamajumder9810@gmail.com>
2024-09-02 23:59:40 +0530

    mmmm

--- src/Convert_an_array/Examples03.java
+++ src/Convert_an_array/Examples03.java
@@ -1,10 +1,21 @@
 package Convert_an_array;
 
+import java.util.Scanner;
+
 public class Examples03 {
     public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int next = scanner.nextInt();
+        System.out.println("Wel-come");
+        System.out.println("HE-JE");
+        System.out.println("FE-DD");
+        System.out.println("ID ");
         System.out.println("Wel-come");
         System.out.println("HE-JE");
         System.out.println("FE-DD");
         System.out.println("ID ");
     }
 }
+
+
