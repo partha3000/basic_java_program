5b692731f59b138cedeb151a334d890fbe528a8f
Partha Majumder <parthamajumder9810@gmail.com>
2024-10-03 23:58:25 +0530

    hellos

--- src/javaProject/Exercise_in_java/ConditionalStatement/Exercise03.java
+++ src/javaProject/Exercise_in_java/ConditionalStatement/Exercise03.java
@@ -1,22 +1,31 @@
 package javaProject.Exercise_in_java.ConditionalStatement;
 
+import java.util.Scanner;
+
 public class Exercise03 {
-    public static void main(String [] arg){
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        double x = in.nextDouble();
+        System.out.print("Input the second number: ");
+        double y = in.nextDouble();
+        System.out.print("Input the third number: ");
+        double z = in.nextDouble();
+        System.out.print("The average value is " + average(x, y, z)+"\n" );
+    }
 
-            {
-                Scanner in = new Scanner(System.in);
-                System.out.print("Input the first number: ");
-                double x = in.nextDouble();
-                System.out.print("Input the second number: ");
-                double y = in.nextDouble();
-                System.out.print("Input the third number: ");
-                double z = in.nextDouble();
-                System.out.print("The average value is " + average(x, y, z)+"\n" );
-            }
+    public static double average(double x, double y, double z)
+    {
+        return (x + y + z) / 3;
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");System.out.println("Hello World");
 
-            public static double average(double x, double y, double z)
-            {
-                return (x + y + z) / 3;
-            }
-        }
+
+    }
 }
