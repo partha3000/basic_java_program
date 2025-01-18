2c7e58d2f20325545407c1e1957618a27330a6fc
Partha Majumder <parthamajumder9810@gmail.com>
2025-01-18 23:20:40 +0530

    convert octal number to decimal number

---
+++ src/javaProject/Basic_Java/JavaDemo10.java
@@ -0,0 +1,32 @@
+/*
+Experiment Number : 25
+Experiment Name : Write a Java program to convert a octal number to a decimal number.
+ */
+package javaProject.Basic_Java;
+
+import java.util.Scanner;
+
+public class JavaDemo10 {
+    public static void main (String [] args){
+        // Create a Scanner object to read input from the user
+        Scanner in = new Scanner(System.in);
+
+        // Declare variables to store octal and decimal numbers, and an index counter
+        long octal_num, decimal_num = 0;
+        int i = 0;
+
+        // Prompt the user to input an octal number
+        System.out.print("Input any octal number: ");
+        octal_num = in.nextLong();
+
+        // Convert the octal number to decimal
+        while (octal_num != 0) {
+            decimal_num = (long)(decimal_num + (octal_num % 10) * Math.pow(8, i++));
+            octal_num = octal_num / 10;
+        }
+
+        // Display the equivalent decimal number
+        System.out.print("Equivalent decimal number: " + decimal_num + "\n");
+
+    }
+}
