79ef7fe1ffaf65e3a8270bbd70f6ec43f66e1f30
Partha Majumder <parthamajumder9810@gmail.com>
2025-02-02 00:09:13 +0530

    The area of a polugon

---
+++ src/javaProject/Basic_Java/JavaDemo20.java
@@ -0,0 +1,31 @@
+/*
+Experiment Number :
+Experiment Name : Write a java program to compute the area of a polygon
+ */
+package javaProject.Basic_Java;
+
+import java.util.Scanner;
+
+public class JavaDemo20 {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input the number of sides on the polygon:");
+
+        //read the number od side from the user.
+        int ns = scanner.nextInt();
+
+        //Prompt the user to input the length of one of the side.
+        System.out.println("Input the length of one of the side: ");
+
+        //Read the length of one side from the user.
+        double side = scanner.nextDouble();
+
+        // Calculate and display the area of the polygon
+        System.out.println("The area is : " + polygonArea (ns, side) + "\n");
+        scanner.close();
+    }
+    public static double polygonArea (int ns, double side){
+        return  (ns * (side * side)) / (4.0 * Math.tan((Math.PI/ns)));
+    }
+}
