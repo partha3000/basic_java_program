3b7ac46fe6ae97c3e91ad88d06b432c342fec3c7
Partha Majumder <parthamajumder9810@gmail.com>
2025-02-17 23:37:31 +0530

    the middle character of a string

---
+++ src/javaProject/Method/Experiment3.java
@@ -0,0 +1,28 @@
+package javaProject.Method;
+
+import java.util.Scanner;
+
+public class Experiment3 {
+    public static void main (String [] arg){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String str = in.nextLine();
+        System.out.print("The middle character in the string: " + middle(str)+"\n");
+    }
+    public static String middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+        return str.substring(position, position + length);
+    }
+}
