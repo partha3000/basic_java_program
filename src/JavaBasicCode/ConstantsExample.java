8fff57059fe644ade475f783f7fd7d4475c21966
Partha Majumder <parthamajumder9810@gmail.com>
2025-04-03 05:30:24 +0530

    Constants in java programming

---
+++ src/JavaBasicCode/ConstantsExample.java
@@ -0,0 +1,9 @@
+package JavaBasicCode;
+
+public class ConstantsExample {
+    public static final  double PI = 3.14159;
+
+    public static void main (String [] arg){
+        System.out.println("value is : " + PI);
+    }
+}
