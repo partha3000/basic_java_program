452d29439580b11947c4e28f819e80241e81a17c
Partha Majumder <parthamajumder9810@gmail.com>
2025-04-14 23:30:21 +0530

    new code

--- src/Convert_an_array/StackJava/Stack02.java
+++ src/Convert_an_array/StackJava/Stack02.java
@@ -87,13 +87,7 @@ public class Stack02 {
         System.out.println("Initialize a stack:");
         Stack stack = new Stack(6);
         System.out.println("\nInput some elements on the stack:");
-        stack.push(1);
-        stack.push(3);
-        stack.push(2);
-        stack.push(0);
-        stack.push(7);
-        stack.push(5);
-        stack.display();
+
         System.out.println("\nSort the elements of the stack in ascending order:");
         stack.sort();
         stack.display();
