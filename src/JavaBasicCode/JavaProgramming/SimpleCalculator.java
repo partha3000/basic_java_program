ef431eaebbcbaa7826f0abc0b65e46ac521ec4fd
Partha Majumder <parthamajumder9810@gmail.com>
2025-04-19 12:44:48 +0530

    simpale Calculator

---
+++ src/JavaBasicCode/JavaProgramming/SimpleCalculator.java
@@ -0,0 +1,48 @@
+package JavaBasicCode.JavaProgramming;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the an operator(+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        double result ;
+
+        switch (operator){
+            case '+':
+            result = num1 + num2;
+                System.out.println("Result is :" + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result is :" + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("result is :" + result);
+                break;
+            case '/':
+                if (num2 !=0){
+                    result = num1 / num2;
+                    System.out.println("result is :" + result);
+                } else {
+                    System.out.println("Error: cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+                break;
+
+        }
+        scanner.close();
+    }
+}
