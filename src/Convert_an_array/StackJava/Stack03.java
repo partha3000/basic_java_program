95fad3a65cffebb1f0d377c124a3b981aa2ee033
Partha Majumder <parthamajumder9810@gmail.com>
2025-05-15 00:44:34 +0530

    new jV program

--- src/Convert_an_array/StackJava/Stack03.java
+++ src/Convert_an_array/StackJava/Stack03.java
@@ -1,84 +1,22 @@
 package Convert_an_array.StackJava;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Stack03 {
-    private int[] arr;
-    private int top;
-
-    // Constructor to initialize the stack
-    public void Stack(int size) {
-        arr = new int[size];
-        top = -1;
-    }
-
-    // Method to push an element onto the stack
-    public void push(int num) {
-        if (top == arr.length - 1) {
-            System.out.println("Stack is full");
-        } else {
-            top++;
-            arr[top] = num;
-        }
-    }
-
-    // Method to pop an element from the stack
-    public int pop() {
-        if (top == -1) {
-            System.out.println("Stack Underflow");
-            return -1;
-        } else {
-            int poppedElement = arr[top];
-            top--;
-            return poppedElement;
-        }
-    }
-
-    // Method to get the top element of the stack
-    public int peek() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
-            return -1;
-        } else {
-            return arr[top];
-        }
-    }
-
-    // Method to check if the stack is empty
-    public boolean isEmpty() {
-        return top == -1;
-    }
-
-    public void reverse() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
-            return;
-        }
-
-        int[] reversedArray = new int[arr.length];
-        int index = 0;
-
-        while (!isEmpty()) {
-            int temp = pop();
-            reversedArray[index++] = temp;
-        }
-
-        for (int i = 0; i < index; i++) {
-            push(reversedArray[i]);
-        }
-    }
-    public void display() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
-        } else {
-            System.out.print("Stack elements: ");
-            for (int i = top; i >= 0; i--) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.println("Initialize a stack:");
+        // Creating a Scanner object to take input from the user
+        Scanner in = new Scanner(System.in);
 
+        // Prompting the user to input the first number
+        System.out.print("Input first number: ");
+        double n1 = in.nextDouble();
+
+        // Prompting the user to input the second number
+        System.out.print("Input second number: ");
+        double n2 = in.nextDouble();
+
+        // Checking if both numbers are greater than 0 and less than 1
+        System.out.println(n1 > 0 && n1 < 1 && n2 > 0 && n2 < 1);
     }
 }
