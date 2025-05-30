ce52d9e804a880fbc98ff80cb17b238dd435be3f
Partha Majumder <parthamajumder9810@gmail.com>
2025-05-31 00:37:52 +0530

    the code is reday

--- src/Convert_an_array/StackJava/Stack04.java
+++ src/Convert_an_array/StackJava/Stack04.java
@@ -45,3 +45,23 @@ public class Stack04 {
     }
 
 }
+// Private instance variables
+private String name;
+private String color;
+
+// Parameterized constructor
+public Dog(String name, String color) {
+    // Initialize name with the provided parameter
+    this.name = name;
+    // Initialize color with the provided parameter
+    this.color = color;
+}
+
+// Main method to test the Dog class
+public static void main(String[] args) {
+    // Create a new Dog object using the parameterized constructor
+    Dog myDog = new Dog("Bailey", "Black");
+    // Print the values of the instance variables
+    System.out.println("Dog's Name: " + myDog.name);
+    System.out.println("Dog's Color: " + myDog.color);
+}
