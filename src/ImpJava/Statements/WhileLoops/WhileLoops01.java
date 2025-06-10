02b53e674e226412e4444ca4763da9af01d4437e
Partha Majumder <parthamajumder9810@gmail.com>
2025-06-10 10:40:56 +0530

    do while loops

---
+++ src/ImpJava/Statements/WhileLoops/WhileLoops01.java
@@ -0,0 +1,12 @@
+package ImpJava.Statements.WhileLoops;
+
+public class WhileLoops01 {
+    public static void main(String[] args) {
+        int i =5;
+
+        do {
+            System.out.println("this is a do-while loop. ");
+            i--;
+        }while (i < 0);
+    }
+}
